d99d27a58427860bcc0b20e68f043dc0ed919721
Jay Potter <j@ypotter.ca>
2024-03-31 18:02:35 -0400

    Create ResourceAwareInterface.php

---
+++ src/Potter/Resource/Aware/ResourceAwareInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Resource\Aware;
+
+interface ResourceAwareInterface
+{
+    public function getResource(): mixed;
+    public function hasResource(): bool;
+}
